a41fabdaebd2343ce19b7ab795f13efc2ccc931e
Vitor Baptista <vitor@vitorbaptista.com>
2016-04-18 11:37:37 +0100

    Revert "Deprecated this project. Datapackage-py should be used instead."
    Reverting deletion of this project's code to allow pushing new version to PyPI
    with deprecation notice.
    
    This reverts commit 91ca1539d5fbd516bb9e0785cf9f262b86f88c51.

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
+[![Build Status](https://travis-ci.org/okfn/datapackage-validate-py.svg)](https://travis-ci.org/okfn/datapackage-validate-py) [![Coverage Status](https://coveralls.io/repos/okfn/datapackage-validate-py/badge.svg?branch=master&service=github)](https://coveralls.io/github/okfn/datapackage-validate-py?branch=master)
+
 # datapackage-validate-py
 
-This project is **DEPRECATED**. You should use
-<https://github.com/datapackages/datapackage-py> instead.
+Validate [Data Package][] datapackage.json files against a jsonschema.
+
+[Data Package]: http://data.okfn.org/doc/data-package
+
+## Usage
+
+```python
+import datapackage_validate
+
+try:
+  datapackage_validate.validate(datapackage, schema)
+except datapackage_validate.exceptions.DataPackageValidateException as e:
+  e.errors  # List with validation errors
+```
+
+The `datapackage` can be a json string or python dict.
+
+The `schema` can be a json string, python dict, or a schema id corresponding with a schema from the registry of [Data Package Profiles][]. `schema` is optional, and will default to the `base` schema id if not provided.
+
+`validate()` returns None. If there were errors during validation, it raises a
+`datapackage_validate.exceptions.DataPackageValidateException` with a list of
+the validation errors in its `.errors` property.
+
+[Data Package Profiles]: https://github.com/dataprotocols/registry
---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import io
+
+from setuptools import setup, find_packages
+
+description = "A Python library to validate Data Package datapackage.json files."
+with io.open('README.md') as readme:
+    long_description = ''.join(
+        filter(lambda x: 'https://travis-ci.org/' not in x,
+               readme.readlines()))
+
+dependencies = [
+    'requests>=2.8.0',
+    'jsonschema>=2.5.1',
+    'datapackage-registry>=0.3.0'
+]
+
+setup(
+    name='datapackage-validate',
+    version='0.3.1',
+    url='https://github.com/okfn/datapackage-validate-py',
+    license='MIT',
+    description=description,
+    long_description=long_description,
+    author='Open Knowledge Foundation',
+    author_email='info@okfn.org',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    package_dir={'datapackage_validate': 'datapackage_validate'},
+    install_requires=dependencies,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
